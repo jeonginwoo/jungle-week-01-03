4db5acb917c647eefa75da90160c4f920ec4b878
inwoo <jiw413@naver.com>
2024-08-09 12:52:20 +0900

    문제24 - 종이자르기.py

---
+++ week01/문제24 - 종이자르기.py
@@ -0,0 +1,27 @@
+"""
+출처 : 백준
+번호 : 2628
+난이도 : 실버 5
+제목 : 종이자르기
+"""
+
+width, height = [int(x) for x in input().split()]
+N = int(input())
+col = [0]
+row = [0]
+for _ in range(N):
+    select, idx = [int(x) for x in input().split()]
+    if select == 0:
+        row.append(idx)
+    else:
+        col.append(idx)
+col.sort()
+row.sort()
+col += [width]
+row += [height]
+area = []
+
+for c in range(1, len(col)):
+    for r in range(1, len(row)):
+        area.append((col[c] - col[c - 1]) * (row[r] - row[r - 1]))
+print(max(area))
