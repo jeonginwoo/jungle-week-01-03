c8a978eb5f07fcaed9540100fc9b3dff97d89036
inwoo <jiw413@naver.com>
2024-08-09 10:46:41 +0900

    문제21 - 소수 찾기

---
+++ week01/문제21 - 소수 찾기.py
@@ -0,0 +1,26 @@
+"""
+출처 : 백준
+번호 : 1978
+난이도 : 브론즈 2
+제목 : 소수 찾기
+"""
+
+def isDecimal(num):
+    if num == 1:
+        return False
+    div = 2
+    half = num/2
+    while div <= half:
+        if num % div == 0:
+            return False
+        div += 1
+    return True
+
+n = int(input())
+numList = [int(x) for x in input().split()]
+
+count = 0
+for num in numList:
+    if isDecimal(num):
+        count += 1
+print(count)
