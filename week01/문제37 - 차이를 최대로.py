38f36aa2f6ba1d355f3f8bac85ffb6bc7918d957
inwoo <jiw413@naver.com>
2024-08-09 19:48:18 +0900

    문제37 - 차이를 최대로

---
+++ week01/문제37 - 차이를 최대로.py
@@ -0,0 +1,34 @@
+"""
+출처 : 백준
+번호 : 10819
+난이도 : 실버 2
+제목 : 차이를 최대로
+"""
+
+
+def diffSum(numList):
+    sum = 0
+    for i in range(len(numList) - 1):
+        sum += abs(numList[i] - numList[i + 1])
+    return sum
+
+
+def shuffle(n, numList, shuf, maxSum):
+    if n == 0:
+        maxSum[0] = max(maxSum[0], diffSum(shuf))
+        return
+    for num in range(len(numList)):
+        tempNumList = numList[:]
+        tempShuf = shuf[:]
+        tempShuf.append(tempNumList.pop(num))
+        shuffle(n - 1, tempNumList, tempShuf, maxSum)
+
+
+N = int(input())
+numList = [int(x) for x in input().split()]
+
+temp = numList[:]
+shuf = []
+maxSum = [0]
+shuffle(N, numList, shuf, maxSum)
+print(maxSum[0])
