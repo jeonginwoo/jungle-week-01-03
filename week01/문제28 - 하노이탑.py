86288ebe144229648396b1ee2f5f44acce2906ce
inwoo <jiw413@naver.com>
2024-08-10 17:18:22 +0900

    문제28 - 하노이탑.py

---
+++ week01/문제28 - 하노이탑.py
@@ -0,0 +1,22 @@
+"""
+출처 : 백준
+번호 : 1914
+난이도 : 골드 5
+제목 : 하노이탑
+"""
+
+
+def hanoi(N, now, next):
+    global moveList
+    if N == 0:
+        return
+    hanoi(N - 1, now, 6 - now - next)
+    print(now, next)
+    hanoi(N - 1, 6 - now - next, next)
+
+
+N = int(input())
+moveList = []
+print(2**N - 1)
+if N <= 20:
+    hanoi(N, 1, 3)
