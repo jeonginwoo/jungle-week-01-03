10fbf080af0d7742f6a1498fdc402e48cf2b6dba
inwoo <jiw413@naver.com>
2024-08-11 13:31:30 +0900

    문제30 - Z

--- week01/문제30 - Z.py
+++ week01/문제30 - Z.py
@@ -8,8 +8,8 @@
 
 def Z(N, x, y):
     global count, r, c
-    nextX = x - 2 ** (N-1)
-    nextY = y - 2 ** (N-1)
+    nextX = x - 2 ** (N - 1)
+    nextY = y - 2 ** (N - 1)
 
     if r <= nextX and c <= nextY:
         if N == 1:
@@ -18,17 +18,17 @@ def Z(N, x, y):
     elif r <= nextX and c > nextY:
         if N == 1:
             return 1
-        return Z(N - 1, nextX, y) + 4 ** (N-1)
+        return Z(N - 1, nextX, y) + 4 ** (N - 1)
     elif r > nextX and c <= nextY:
         if N == 1:
             return 2
-        return Z(N - 1, x, nextY) + 2 * 4 ** (N-1)
+        return Z(N - 1, x, nextY) + 2 * 4 ** (N - 1)
     else:
         if N == 1:
             return 3
-        return Z(N - 1, x, y) + 3 * 4 ** (N-1)
+        return Z(N - 1, x, y) + 3 * 4 ** (N - 1)
 
 
 N, r, c = [int(x) for x in input().split()]
 count = 0
-print(Z(N, x=2**N-1, y=2**N-1))
+print(Z(N, x=2 ** N - 1, y=2 ** N - 1))
