60952aaf32d569f015cc276351e05819a2f5f3f1
inwoo <jiw413@naver.com>
2024-08-12 22:07:28 +0900

    문제33 - 수 정렬하기 3 (도수 정렬)
    드으으디어 통과

---
+++ week01/문제33 - 수 정렬하기 3 (도수 정렬).py
@@ -0,0 +1,23 @@
+"""
+출처 : 백준
+번호 : 10989
+난이도 : 브론즈 1
+제목 : 수 정렬하기 3
+"""
+
+# 메모리 초과
+
+import sys
+
+maxVal = 10000
+count = [0] * (maxVal + 1)
+
+input = sys.stdin.readline
+N = int(input())
+
+for _ in range(N):
+    count[int(input())] += 1
+
+for num in range(1, maxVal + 1):
+    for _ in range(count[num]):
+        print(num)
