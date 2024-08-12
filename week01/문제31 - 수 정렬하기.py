e3458b0bbfb238de84cde7e918f272a7baadaae3
inwoo <jiw413@naver.com>
2024-08-12 15:36:29 +0900

    문제31 - 수 정렬하기
    python 내장함수 대신 퀵 정렬 사용

--- week01/문제31 - 수 정렬하기.py
+++ week01/문제31 - 수 정렬하기.py
@@ -5,21 +5,35 @@
 제목 : 수 정렬하기
 """
 
+import sys
+
+sys.setrecursionlimit(15000)
+
 
 def quickSort(numList, first, last):
+    if first >= last:
+        return
     p = numList[first]
     left = first + 1
     right = last
     while left <= right:
-
-    return
+        while left <= right and numList[left] < p:
+            left += 1
+        while numList[right] > p:
+            right -= 1
+        if left <= right:
+            numList[left], numList[right] = numList[right], numList[left]
+            left += 1
+            right -= 1
+    numList[first], numList[right] = numList[right], numList[first]
+    quickSort(numList, first, right - 1)
+    quickSort(numList, left, last)
 
 
 N = int(input())
 numList = []
 for _ in range(N):
     numList.append(int(input()))
-
-quickSort(numList)
+quickSort(numList, 0, N - 1)
 for num in numList:
     print(num)
