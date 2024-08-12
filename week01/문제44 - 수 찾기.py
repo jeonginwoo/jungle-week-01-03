e9d23589d17e807a449e275d92467dabbeaa02d9
inwoo <jiw413@naver.com>
2024-08-12 14:38:08 +0900

    문제44 - 수 찾기
    이분탐색으로 다시 풀어봄

--- week01/문제44 - 수 찾기.py
+++ week01/문제44 - 수 찾기.py
@@ -6,13 +6,43 @@
 """
 
 
+# # 집합(해시셋)
+#
+# N = int(input())
+# numSet = set([int(x) for x in input().split()])
+# M = int(input())
+# checkList = [int(x) for x in input().split()]
+#
+# for num in checkList:
+#     if num in numSet:
+#         print(1)
+#     else:
+#         print(0)
+
+
+# 이분 탐색
+
+def checkNum(find, numList):
+    start = 0
+    end = len(numList) - 1
+    while start != end:
+        now = (start + end) // 2
+        if numList[now] < find:
+            start = now + 1
+        elif numList[now] > find:
+            end = now
+        else:
+            start = end = now
+    if find == numList[start]:
+        print(1)
+    else:
+        print(0)
+
+
 N = int(input())
-numSet = set([int(x) for x in input().split()])
+numList = sorted([int(x) for x in input().split()])
 M = int(input())
 checkList = [int(x) for x in input().split()]
 
 for num in checkList:
-    if num in numSet:
-        print(1)
-    else:
-        print(0)
+    checkNum(num, numList)
