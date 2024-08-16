9381975ac68e6bdb17b5a048aff2c34283b5c33b
inwoo <jiw413@naver.com>
2024-08-16 17:09:50 +0900

    문제14 - 최대 힙

---
+++ week02/문제14 - 최대 힙.py
@@ -0,0 +1,23 @@
+"""
+출처 : 백준
+번호 : 11279
+난이도 : 실버 2
+제목 : 최대 힙
+"""
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+def makeMaxHeap(L):
+
+
+N = int(input())
+max_heap = []
+for _ in range(N):
+    num = int(input())
+    if num == 0:
+        print(0 if len(max_heap) == 0 else -heapq.heappop(max_heap))
+    else:
+        heapq.heappush(max_heap, -num)
+
