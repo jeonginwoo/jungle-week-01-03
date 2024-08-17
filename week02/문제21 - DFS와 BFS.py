a577417a115925dc063f033fd94b15b28eb4caa0
inwoo <jiw413@naver.com>
2024-08-17 16:53:48 +0900

    문제21 - DFS와 BFS

---
+++ week02/문제21 - DFS와 BFS.py
@@ -0,0 +1,53 @@
+"""
+출처 : 백준
+번호 : 1260
+난이도 : 실버 2
+제목 : DFS와 BFS
+"""
+
+import sys
+from collections import deque
+
+def DFS(edges, V):
+    visited = [False] * (len(edges) + 1)
+    visit = []
+    stack = [V]
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            visited[v] = True
+            visit.append(v)
+            next_list = edges[v]
+            for next in range(len(next_list)-1, -1, -1):
+                if not visited[next_list[next]]:
+                    stack.append(next_list[next])
+    return visit
+
+def BFS(edges, V):
+    visited = [False] * (len(edges) + 1)
+    visit = []
+    queue = deque()
+    queue.append(V)
+    while queue:
+        v = queue.popleft()
+        if not visited[v]:
+            visited[v] = True
+            visit.append(v)
+            next_list = edges[v]
+            for next in next_list:
+                if not visited[next]:
+                    queue.append(next)
+    return visit
+
+input = sys.stdin.readline
+
+N, M, V = map(int, input().split())
+edges = [[] for _ in range(N+1)]
+for i in range(M):
+    arr = list(map(int, input().split()))
+    edges[arr[0]].append(arr[1])
+    edges[arr[1]].append(arr[0])
+for i in range(1, N+1):
+    edges[i].sort()
+print(*DFS(edges, V), sep=" ")
+print(*BFS(edges, V), sep=" ")
