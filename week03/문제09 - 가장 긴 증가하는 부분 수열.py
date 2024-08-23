5b1a5519215f2bc7583b0af39a4d1e7663ccf72c
inwoo <jiw413@naver.com>
2024-08-23 17:14:10 +0900

    문제09 - 가장 긴 증가하는 부분 수열

---
+++ week03/문제09 - 가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,19 @@
+"""
+출처 : 백준
+번호 : 11053
+난이도 : 실버 2
+제목 : 가장 긴 증가하는 부분 수열
+"""
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+dp = [1] * N
+
+for i in range(1, N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
