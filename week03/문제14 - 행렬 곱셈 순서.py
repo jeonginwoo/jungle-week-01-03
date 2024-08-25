320d1f172e106b345305e504f82fad2fb4dc99be
inwoo <jiw413@naver.com>
2024-08-25 12:18:37 +0900

    문제14 - 행렬 곱셈 순서

---
+++ week03/문제14 - 행렬 곱셈 순서.py
@@ -0,0 +1,27 @@
+"""
+출처 : 백준
+번호 : 11049
+난이도 : 골드 3
+제목 : 행렬 곱셈 순서
+"""
+
+# python3 시간 초과
+# pypy3로 통과
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+matrices = [list(map(int, x.split())) for x in sys.stdin.read().splitlines()]
+
+dp = [[0] * N for _ in range(N)]
+for i in range(1, N):
+    for j in range(i, N):
+        left, right = j - i, j
+        dp[left][right] = float("inf")
+        for k in range(left, right):
+            cost = dp[left][k] + dp[k+1][right] + matrices[left][0] * matrices[k][1] * matrices[right][1]
+            if cost < dp[left][right]:
+                dp[left][right] = cost
+
+print(dp[0][N - 1])
